8479db6984e194b474feac53ad4fbda3fa28773d
Timo Kaiser <34896723+TimoK93@users.noreply.github.com>
2023-11-21 15:52:18 +0100

    fixed bug

--- ctc_metrics/__init__.py
+++ ctc_metrics/__init__.py
@@ -1,2 +1,2 @@
-from scripts.evaluate import evaluate_sequence
-from scripts.validate import validate_sequence
+from ctc_metrics.scripts.evaluate import evaluate_sequence
+from ctc_metrics.scripts.validate import validate_sequence
