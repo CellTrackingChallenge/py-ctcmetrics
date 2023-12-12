de9457c8dcbf12ac211b5f5e284fa2f8182b66e6
Timo Kaiser <34896723+TimoK93@users.noreply.github.com>
2023-12-12 16:21:48 +0100

    Changed test dataset

--- test/utils.py
+++ test/utils.py
@@ -4,9 +4,9 @@ test_seq_res = "test_dataset_ctc/train/BF-C2DL-HSC/01_RES"
 test_seq_gt = "test_dataset_ctc/train/BF-C2DL-HSC/01_GT"
 
 
-test_det = 0.951931
-test_seg = 0.903821
-test_tra = 0.957398
+test_det = 0.95377521
+test_seg = 0.903990207454052
+test_tra = 0.9588616
 test_ct = 0.0190476
 test_tf = 0.72417699
 test_bc0 = 0.434782
