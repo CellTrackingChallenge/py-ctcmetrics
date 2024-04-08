4bdbba77dd6550ea2cf59d282db1074489bdd877
Timo Kaiser <34896723+TimoK93@users.noreply.github.com>
2024-04-08 12:02:42 +0200

    Fixed bug sources and allow empty masks

--- ctc_metrics/utils/representations.py
+++ ctc_metrics/utils/representations.py
@@ -29,6 +29,11 @@ def match(
         label pair.
     """
     # Read the input data
+    if comp_path is None:
+        map_ref = tiff.imread(ref_path)
+        labels_ref = np.unique(map_ref)
+        labels_ref = labels_ref[labels_ref != 0]
+        return labels_ref.tolist(), [], [], [], []
     if ref_path is None:
         # For trivial cases where only one mask should be analysed
         ref_path = comp_path
@@ -56,6 +61,8 @@ def match(
     if ref_path == comp_path:
         # For trivial cases where only one mask should be analysed
         iou = np.ones(len(labels_ref))
+        labels_ref = labels_ref[labels_ref != 0]
+        labels_comp = labels_comp[labels_comp != 0]
         return labels_ref.tolist(), labels_comp.tolist(), labels_ref.tolist(),\
             labels_comp.tolist(), iou.tolist()
     # Add offset to separate the labels of the two masks
@@ -153,7 +160,7 @@ def create_edge_mapping(
             [ind1, label1, det_test1, t1, ind2, label2, det_test2, t2, 1]
         )[None, :]
         all_edges.append(edges)
-    return np.concatenate(all_edges, axis=0)
+    return np.concatenate(all_edges, axis=0).astype(int)
 
 
 def create_detection_test_matrix(
