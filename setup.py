f99a2042ddd5e64e5526efe51ee5a2322820d03d
Timo Kaiser <34896723+TimoK93@users.noreply.github.com>
2024-08-21 14:30:33 +0200

    Changed PyPI badge and Version name for publishing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="py-ctcmetrics",
-    version="0.1.4",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
         "numpy",
