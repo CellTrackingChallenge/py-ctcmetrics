4ad0937aaae769662bf34600ea4515026fed0db2
Timo Kaiser <34896723+TimoK93@users.noreply.github.com>
2024-10-11 15:55:32 +0200

    Set new Version for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="py-ctcmetrics",
-    version="1.0.1",
+    version="1.1.0",
     packages=find_packages(),
     install_requires=[
         "numpy",
